18e9f739b303eb74ec28db42f371020d7c7a839c
Ítalo de Andrade <italolande@gmail.com>
2024-10-30 15:33:25 -0300

    contador

---
+++ grafo.py
@@ -0,0 +1,36 @@
+import time
+import os
+import sys
+from time import sleep
+from multiprocessing import Process
+
+def cont(n, t, nome):
+    print(f"processo: {nome} iniciado")
+    for i in range(n):
+        print(f"{nome} : {i+1} ")
+        time.sleep(t)
+    print("processo encerrado")
+
+def main():
+    numero= int(input("insira o numero: "))
+    tempo= float(input("insira o tempo: "))
+
+    A = Process(target=cont, args=(numero, tempo, "A"))
+    B = Process(target=cont, args=(numero, tempo, " B"))
+    C = Process(target=cont, args=(numero, tempo, "  C"))
+    D = Process(target=cont, args=(numero, tempo, "   D"))
+    
+    A.start()
+    B.start()
+    C.start()
+    D.start()
+
+    print("Aguardando fim dos processos")
+    A.join()
+    B.join()
+    C.join()
+    D.join()
+
+
+if __name__ == '__main__':
+    main()
