1e0429d68912dd0b321cf22ac0cbac6d3771fe14
Ítalo de Andrade <italolande@gmail.com>
2024-11-03 18:39:35 -0300

    update

--- grafo.py
+++ grafo.py
@@ -2,8 +2,8 @@ import time
 from multiprocessing import Process, Semaphore
 
 def cont(n, t, nome, sem=None, sem_sai=None):
-    print(f"Processo {nome} iniciado")
-    
+    print(f"Processo {nome} iniciou contagem")
+    10
     if sem:
         sem.acquire()  
 
@@ -15,21 +15,21 @@ def cont(n, t, nome, sem=None, sem_sai=None):
         sem_sai.release()
         sem_sai.release()
 
-    print(f"Processo {nome} encerrado")
+    print(f"Processo {nome} finalizou contagem")
         
 
 def main():
     numero = int(input("Insira o número: "))
     tempo = float(input("Insira o tempo: "))
 
-    sem_BC = Semaphore(0)
+    sem_A = Semaphore(0)
     sem_D = Semaphore(0)
 
     
-    A = Process(target=cont, args=(numero, tempo, "A",None,sem_BC )) 
-    B = Process(target=cont, args=(numero, tempo, " B",sem_BC,None))  
-    C = Process(target=cont, args=(numero, tempo, "  C",sem_BC,sem_D))
-    D = Process(target=cont, args=(numero, tempo, "   D",sem_D,None))  
+    A = Process(target=cont, args=(numero, tempo, "A",None,sem_A )) 
+    B = Process(target=cont, args=(numero, tempo, " B",sem_A,None))  
+    C = Process(target=cont, args=(numero, tempo, "  C",sem_A,sem_D))
+    D = Process(target=cont, args=(numero, tempo, "D",sem_D,None))  
 
     A.start()
     B.start()
