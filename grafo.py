3a596b415cbb6f4a4f6a05e4e4a018ddf120180b
Ítalo de Andrade <italolande@gmail.com>
2024-11-07 16:04:00 -0300

    atuali

--- grafo.py
+++ grafo.py
@@ -1,11 +1,11 @@
 import time
 from multiprocessing import Process, Semaphore
 
-def cont(n, t, nome, sem=None, sem_sai=None):
+def cont(n, t, nome, sem=None,sem2=None, sem_sai=None):
     print(f"Processo {nome} iniciou contagem")
-    10
-    if sem:
-        sem.acquire()  
+    sem.acquire()
+    if sem2:
+        sem.acquire() 
 
     for i in range(n):
         print(f"{nome} : {i+1}")
@@ -15,28 +15,28 @@ def cont(n, t, nome, sem=None, sem_sai=None):
         sem_sai.release()
         sem_sai.release()
 
+
     print(f"Processo {nome} finalizou contagem")
-        
 
 def main():
     numero = int(input("Insira o número: "))
     tempo = float(input("Insira o tempo: "))
 
-    sem_A = Semaphore(0)
+    sem_A = Semaphore(1)
+    sem_BC= Semaphore(0)
     sem_D = Semaphore(0)
-
-    
-    A = Process(target=cont, args=(numero, tempo, "A",None,sem_A )) 
-    B = Process(target=cont, args=(numero, tempo, " B",sem_A,None))  
-    C = Process(target=cont, args=(numero, tempo, "  C",sem_A,sem_D))
-    D = Process(target=cont, args=(numero, tempo, "D",sem_D,None))  
+     
+    A = Process(target=cont, args=(numero, tempo, "A",sem_A,None,sem_BC )) 
+    B = Process(target=cont, args=(numero, tempo, " B",sem_BC,None,sem_D))
+    C = Process(target=cont, args=(numero, tempo, "  C",sem_BC,None,sem_BC))
+    D = Process(target=cont, args=(numero, tempo, "D",sem_D,sem_BC,None))  
 
     A.start()
     B.start()
     C.start()
     D.start()  
 
- 
+
     A.join()  
     B.join()  
     C.join()
